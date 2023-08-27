77510bf7388805ccba4e9fa88dec5096a62af229
JuliaRoughley <roughleyjulia@gmail.com>
2023-08-27 12:27:08 +0100

    All functioning as expected - last bit before attempting bonus exercise

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ let recipeForm = document.getElementById('recipe-form'); //Here we're grabbing r
 let recipeName = document.getElementById('recipe-name');
 let ingredients = document.getElementById('ingredients');
 let steps = document.getElementById('steps');
+let imageUrl = document.getElementById('recipeImage');
 let displayArea = document.getElementById('display-area');
 
 let recipes = []; //to keep track of all the recipes users add
@@ -12,32 +13,55 @@ recipeForm.addEventListener('submit', function(event) {
     let enteredRecipeName = recipeName.value; //grabbing the values from the submitted forms and storing them.
     let enteredIngredients = ingredients.value;
     let enteredSteps = steps.value;
+    let enteredImageUrl = imageUrl.value;
 
     let newRecipe = { //creating the new recipe with the variables in the form
         name: enteredRecipeName,
         ingredients: enteredIngredients,
-        steps: enteredSteps
+        steps: enteredSteps,
+        image: enteredImageUrl
     };
 
     recipes.push(newRecipe); //adding the new recipe to the array of recipes
+    localStorage.setItem('recipes', JSON.stringify(recipes)); //adding to local storage in the browser
 
     recipeName.value = ''; //clearing the form input fields to be used again
     ingredients.value = '';
     steps.value = '';   
+    imageUrl.value = '';
 
     displayRecipe(newRecipe);
 
 });
 
+
+// Check if recipes exist in local storage
+if (localStorage.getItem('recipes')) {
+    // Load recipes from local storage and parse the JSON string
+    recipes = JSON.parse(localStorage.getItem('recipes'));
+
+    // Display the loaded recipes on the page
+    recipes.forEach(recipe => {
+        displayRecipe(recipe);
+    });
+}
+
+
 function displayRecipe(recipe) {
     let recipeDiv = document.createElement('div'); //creating a new div element for the new recipe to be displayed in
 
     let recipeHTML = `
-        <h3>${recipe.name}</h3>
-        <p><strong>Ingredients:</strong> ${recipe.ingredients}</p>
-        <p><strong>Steps:</strong> ${recipe.steps}</p>
-    `;
+    <h3>${recipe.name}</h3>
+    <p><strong>Ingredients:</strong> ${recipe.ingredients}</p>
+    <p><strong>Steps:</strong> ${recipe.steps}
+`;
 
+// Add the image if an image URL is provided
+if (recipe.image) {
+    recipeHTML += `<img src="${recipe.image}" alt="Picture of ${recipe.name}">`;
+}
+
+recipeHTML += `</p>`;
     let deleteButton = document.createElement('button');
     deleteButton.textContent = "Delete Recipe";
     let index = recipes.indexOf(recipe);
@@ -58,6 +82,8 @@ function displayRecipe(recipe) {
 function deleteRecipe(index) {
     // Remove recipe from the array recipes
     recipes.splice(index, 1);
+
+    localStorage.setItem('recipes', JSON.stringify(recipes)); //updating local storage
 
     // Refresh the Display
     refreshDisplay();
